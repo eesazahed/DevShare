a66258974c94fda68a2f778a546dfb343ed06ace
TechLion Dev (via VSCode) <git-bash@techlion.dev>
2023-03-16 17:08:46 -0400

    Add Favicon And Title
    Signed-off-by: TechLion Dev (via VSCode) <git-bash@techlion.dev>

---
+++ components/CHead.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Meta from "./Meta";
+import Favicons from "./Favicons";
+
+const CHead = ({title}) => {
+    return (
+        <>
+            <Head>
+                <title>{title} | DevShare</title>
+                <Meta />
+                <Favicons/>
+            </Head>
+        </>
+    );
+}
+
+export default CHead;
