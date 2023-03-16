f147ae5159fa13a88772da6785b4b2755cfa958f
TechLion Dev (via VSCode) <git-bash@techlion.dev>
2023-03-15 20:33:23 -0400

    Add 2 Utility Functions
    Signed-off-by: TechLion Dev (via VSCode) <git-bash@techlion.dev>

---
+++ utils/Color.js
@@ -0,0 +1,23 @@
+function numToColor(num) {
+    let color = "blue";
+    switch (num) {
+      case 1:
+        color = "red";
+        break;
+      case 2:
+        color = "yellow";
+        break;
+      case 3:
+        color = "green";
+        break;
+      case 4:
+        color = "blue";
+        break;
+      case 5:
+        color = "purple";
+        break;
+    }
+    return color;
+}
+  
+export { numToColor };
---
+++ utils/User.js
@@ -0,0 +1,20 @@
+import { numToColor } from "./Color";
+
+function getAvatarUrl(name) {
+    let names = name.split(" ");
+    let firstInitial = names[0].substring(0, 1).toUpperCase();
+    let lastInitial = names[names.length - 1].substring(0, 1).toUpperCase();
+    let num = randomIntFromInterval(1, 5);
+    let color = numToColor(num);
+    let url = ``;
+    if (!lastInitial) {
+      url = `https://i2.wp.com/d228am55mqbj0t.cloudfront.net/defaults/${color}-${firstInitial}.png`;
+    } else {
+      url = `https://i2.wp.com/d228am55mqbj0t.cloudfront.net/defaults/${color}-${
+        firstInitial + lastInitial
+      }.png`;
+    }
+    return url;
+}
+  
+export { getAvatarUrl };
