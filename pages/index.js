bfdf642e5d42cf2dd2893f14d8228e701d96526b
TechLion Dev (via VSCode) <git-bash@techlion.dev>
2023-03-18 17:26:43 -0400

    Hero Section
    Signed-off-by: TechLion Dev (via VSCode) <git-bash@techlion.dev>

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,27 @@
-import CHead from "@/components/CHead";
-// import Hero from "@/components/Hero";
+import CHead from '@/components/CHead';
+import Hero from '@/components/Hero';
 // import Nav from "@/components/Nav";
 
-import { getCookie, setCookie, getCookies, deleteCookie } from "cookies-next";
+import { getCookie, setCookie, getCookies, deleteCookie } from 'cookies-next';
 
 const Home = () => {
-	if (getCookie("loggedIn")) {
-	  window.location.href = "/app"; //TODO: ask SonicX180 if app route should be /app or /~
-  } 
-
 	return (
 		<>
-			<CHead title={"Home"} />
-			{/* <Nav /> */}
+			<CHead title={'Home'} />
+
+			{/* <Nav loggedIn={getCookie('loggedIn')} /> */}
 			<div className='p-4 m-2'>
-				{/* <Hero /> */}
+				<Hero
+					variant={'noImg'}
+					title={'Welcome to DevShare'}
+					description={
+						"Welcome to DevShare! This is a Free and Open-Source platform for developers to share their coding knowledge, collaborate with others and work together on projects. There are no dumb questions and all developers are invited."
+					} // TODO: Ask SonicX180 about this
+					primaryBtn={'Get Started'}
+					secondaryBtn={'Learn More'}
+					secondaryBtnLink={'/about'}
+					primaryBtnLink={'/register?ref=app'}
+				/>
 			</div>
 		</>
 	);
